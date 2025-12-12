cb44d44714869de1d078c1c636cac56c873e004d
gerson_6061 santos santiago <gerson_6061@bertioga.sp.gov.br>
2025-12-12 13:55:18 -0300

    chore: upgrade jest to v30 and replace jest-chrome with jest-webextension-mock

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,7 @@ module.exports = {
     transform: {
         '^.+\\.jsx?$': 'babel-jest',
     },
+    setupFiles: ['jest-webextension-mock'],
     testMatch: ['**/tests/**/*.test.js'],
     verbose: true,
     setupFilesAfterEnv: ['./jest.setup.js'],
--- jest.setup.js
+++ jest.setup.js
@@ -1,6 +1,22 @@
-Object.assign(global, require('jest-chrome'));
+// Object.assign(global, require('jest-chrome')); // PREVIOUSLY: jest-chrome (removed)
 
 // Polyfill/Mock para chrome.scripting (não incluído nativamente no jest-chrome ainda)
 global.chrome.scripting = /** @type {any} */ ({
     executeScript: jest.fn(),
 });
+
+// Polyfill ensure for chrome.windows if missing
+if (!global.chrome.windows) {
+    global.chrome.windows = /** @type {any} */ ({
+        update: jest.fn(),
+        create: jest.fn(),
+        get: jest.fn(),
+        getAll: jest.fn(),
+        remove: jest.fn(),
+        onFocusChanged: {
+            addListener: jest.fn(),
+            removeListener: jest.fn(),
+            hasListener: jest.fn(),
+        },
+    });
+}
