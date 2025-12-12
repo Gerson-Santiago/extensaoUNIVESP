b21189764742fd16a838fad8ba59f806d7cbfd97
gerson_6061 santos santiago <gerson_6061@bertioga.sp.gov.br>
2025-12-12 14:11:36 -0300

    chore: otimizar configuração do jest com cobertura v8 e clearMocks

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,7 @@
 module.exports = {
     testEnvironment: 'jsdom',
+    coverageProvider: 'v8', // Optimization for Jest 30
+    clearMocks: true, // Cleanup mocks automatically between tests
     transform: {
         '^.+\\.jsx?$': 'babel-jest',
     },
